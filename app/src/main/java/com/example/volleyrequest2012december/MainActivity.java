45e69f5234ff1c28acf83b811e2b6a5d186706a1
Dan Tra <dltra@fcps.edu>
2021-12-06 15:04:00 -0500

    Create Volley Request, format response, display data

--- app/src/main/java/com/example/volleyrequest2012december/MainActivity.java
+++ app/src/main/java/com/example/volleyrequest2012december/MainActivity.java
@@ -3,12 +3,53 @@ package com.example.volleyrequest2012december;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    TextView textView;
+    RequestQueue queue;
+    String URL = "https://mw-demo.sites.tjhsst.edu/data";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView = findViewById(R.id.response);
+        queue = Volley.newRequestQueue(this);
+        StringRequest request = new StringRequest(Request.Method.GET, URL, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                try {
+                    //format response to JSON object called "data"
+                    JSONObject object = new JSONObject("{\"data\":"+response+"}");
+                    //conver object to an array of objects
+                    JSONArray array = object.getJSONArray("data");
+                    //get first object and display its id
+                    textView.setText("id: " + array.getJSONObject(0).getString("id"));
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                System.out.println("volley error"+ error.toString());
+            }
+        });
+        queue.add(request);
     }
 }
